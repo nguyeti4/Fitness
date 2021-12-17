8894bd8700127513acc54cee6f02ccadc6104687
prchoward <81325528+prchoward@users.noreply.github.com>
2021-12-16 16:32:54 -0800

    Changed background and action bar color/theme Spaced out each child layout Finally got the application working smoothly more fixes to come Fitness-Project Price Howard 12/16/2021

--- app/src/main/java/com/example/searchactivity/MainActivityList.kt
+++ app/src/main/java/com/example/searchactivity/MainActivityList.kt
@@ -1,49 +1,88 @@
 package com.example.searchactivity
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.view.LayoutInflater
 import android.widget.EditText
 import android.widget.Spinner
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 
 import com.example.searchactivity.databinding.ActivityMainListBinding
 
 class MainActivityList : AppCompatActivity() {
-    private var _binding : ActivityMainListBinding? = null
+    private var _binding: ActivityMainListBinding? = null
     private val binding get() = _binding
+    private var p = 1
+
     //Arraylist of workout objects meant to store the users input
-    private var workout_list = ArrayList<Workout>()
+    private var workoutList = ArrayList<Workout>()
+    //tracks the text view indice of the child layout that the user wants to input information to
+    private var indice = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //creates an adapter for the activity main list xml file when the app is opened
         _binding = ActivityMainListBinding.inflate(layoutInflater)
         setContentView(binding!!.root)
 
-
-        //button functonality
-        binding!!.addbutton.setOnClickListener{
+        //button functonality for the "add workout button" calls the "addNewView" function
+        binding!!.addbutton.setOnClickListener {
             addNewView()
         }
-
-        binding!!.submitworkoutinfo!!.setOnClickListener {
+        //button functionality for the submit workout info button. calls the "saveData" function
+        binding!!.submitworkoutinfo.setOnClickListener {
             saveData()
 
         }
     }
+
     //Called whens the add new workout  button is pressed, creates new row for the prog
-    private fun addNewView()
-    {
+    private fun addNewView() {
+        //creates an inflater adapter that pulls an row_add_workout.xml view and adds it into the screen
         val inflater = LayoutInflater.from(this).inflate(R.layout.row_add_workout, null)
         binding!!.parentlayoutlinear.addView(inflater, binding!!.parentlayoutlinear.childCount)
+        var v: View?
+        //sets the current indice to track the expected location for user input
+        indice = binding!!.parentlayoutlinear.childCount - 1
+        v = binding!!.parentlayoutlinear.getChildAt(indice)
+        val workout: EditText = v.findViewById(R.id.wrkt_name)
+        Log.i("Indice:", indice.toString())
+        //sets on click listener in the edit text fields of each inflated layout which calls a searchActivityLauncher
+        workout.setOnClickListener {
+            val intent = Intent(this, MainActivitySearch::class.java)
+            searchActivityLauncher.launch(intent)
+        }
+    }
+
+    private fun onClick(view: View) {
+        var v = LayoutInflater.from(this).inflate(R.layout.row_add_workout, null)
+        var workout: EditText = v.findViewById(R.id.wrkt_name)
+        Toast.makeText(this, "Clicked on", Toast.LENGTH_LONG)
+        workout.setOnClickListener {
+            val intent = Intent(this, MainActivitySearch::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT)
+            startActivity(intent)
+        }
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    public fun setIndice(int: Int) {
+        this.indice = int
     }
 
     //called when user chooses to submit data
-    private fun saveData(){
-        workout_list.clear()
+    private fun saveData() {
+        workoutList.clear()
         val count = binding!!.parentlayoutlinear.childCount
-        var v : View?
-
-        for(i in 0 until count) {
+        var v: View?
+        for (i in 0 until count) {
             v = binding!!.parentlayoutlinear.getChildAt(i)
 
             //Change the following line to receiver from Onclick list view in main activity search
@@ -56,14 +95,34 @@ class MainActivityList : AppCompatActivity() {
             Workout.target = target.toString()
 
             //add the user workout information to created array of Workout objects
-            workout_list.add(Workout)
+            workoutList.add(Workout)
         }
     }
 
+    private fun openNewActivity() {
+        val intent = Intent(this, MainActivitySearch::class.java)
+        startActivity(intent)
 
-    override fun onDestroy() {
-        super.onDestroy()
-        _binding = null
+
     }
 
+    private val searchActivityLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            val intent = result.data
+            var count = binding!!.parentlayoutlinear.childCount
+            var v: View?
+            Log.i("Function:", "OnResume")
+            val Workout = intent!!.getStringExtra("Workout")!!
+            Log.i("MainActivityList:", "workout" + Workout)
+            for (i in 0 until count) {
+                v = binding!!.parentlayoutlinear.getChildAt(i)
+                if (i == indice) {
+                    val workoutslot: EditText = v.findViewById((R.id.wrkt_name))
+                    workoutslot.setText(Workout)
+                }
+
+            }
+
+
+        }
 }
--- app/src/main/java/com/example/searchactivity/MainActivitySearch.kt
+++ app/src/main/java/com/example/searchactivity/MainActivitySearch.kt
@@ -1,58 +1,76 @@
 package com.example.searchactivity
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
-import android.widget.ArrayAdapter
-import android.widget.SearchView
+import android.widget.*
 import com.example.searchactivity.databinding.ActivityMainBinding
 
 class MainActivitySearch : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+    private lateinit var list: MainActivityList
+    private var i = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        var workout = arrayOf("Push-ups", "Squats", "Pull-ups", "Bicep Curl", "Chest Dip", "Dips", "Tricep Extension", "Standing Barbell Curl",
+        //The array of workouts
+        val workout = arrayOf("Push-ups", "Squats", "Pull-ups", "Bicep Curl", "Chest Dip", "Dips", "Tricep Extension", "Standing Barbell Curl",
         "Leg Press", "Calf-raise", "Calf raise", "Tricep Kickback", "Bent over row", "Seated row", "Lunge", "Bicycle Crunches", "Leg Raise",
         "Chest Fly", "Incline Push-ups", "Decline Push-Ups", "Chin-ups", "Russian Twist", "Wrist Curls", "Overhead Press", "Military Press",
-        "Front Raise", "Lateral Raise", "Bench Press", "Rear Delt Raise", "Rear Deltoid Raise", "Delt Raise")
+        "Front Raise", "Lateral Raise", "Bench Press", "Rear Delt Raise", "Rear Deltoid Raise", "Delt Raise", "Deadlift")
 
         val userAdapter : ArrayAdapter<String> = ArrayAdapter(
-            this,android.R.layout.simple_list_item_1,
-            workout
-        )
+            this, android.R.layout.simple_list_item_1,
+            workout)
 
         binding.list1.adapter = userAdapter
-
-        binding.list1.setOnClickListener{
-
+        binding.list1.setOnItemClickListener { _, _, index,_->
+            Toast.makeText(this, "Clicked on ${workout[index]} ", Toast.LENGTH_LONG).show()
+            true
+            val intent = Intent(this, MainActivityList::class.java)
+                Log.i("MainAcivitySearch:", "Workout: ${workout[index]}")
+                intent.putExtra("Workout", workout[index])
+                intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT)
+            setResult(RESULT_OK,intent)
+            this.finish()
         }
 
 
-        binding.searchBar.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            binding.searchBar.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
 
 
-            override fun onQueryTextSubmit(query: String?): Boolean {
-                binding.searchBar.clearFocus()
-                if(workout.contains(query)){
-                    userAdapter.filter.filter(query)
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                    binding.searchBar.clearFocus()
+                    if (workout.contains(query)) {
+                        userAdapter.filter.filter(query)
+                    }else{
+                        Toast.makeText(this@MainActivitySearch, "Workout not found", Toast.LENGTH_LONG)
+                    }
+                    return false
                 }
-                return false
-            }
 
-            override fun onQueryTextChange(newText: String?): Boolean {
-                userAdapter.filter.filter(newText)
-                return false
-            }
+                override fun onQueryTextChange(newText: String?): Boolean {
+                    userAdapter.filter.filter(newText)
+                    return false
+                }
+
+                private fun saveData() {
+                    val workout = Workout()
 
 
-
-             } )
+                }
+            })
+        }
     }
-    }
+
+
+
+
 
